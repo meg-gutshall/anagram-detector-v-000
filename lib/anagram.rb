e6bb003f85f4ca7b54ef9df2ffa208acbc6f290a
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-08-16 22:49:34 +0000

    Detect if a word is an anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+
+class Anagram
+  # Set reader and writer for word string and array
+  attr_accessor :word, :w_array
+
+  # Initialize word instance variable
+  def initialize(word)
+    @word = word
+  end
+
+  # Are any of the words in the array an anagram of the word instance?
+  # Iterate over the array with the condition to delete the word object
+  # if its letters don't match the letters from the word instance
+  def match(w_array)
+    w_array.delete_if {|w| w.split("").sort != word.split("").sort}
+  end
+
+end
